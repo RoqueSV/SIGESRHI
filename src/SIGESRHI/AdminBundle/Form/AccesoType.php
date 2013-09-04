910a7e35b9809e4cdceaf30c4a7f7e0eb006c0a1
Roquet87 <erickroquet@gmail.com>
2013-09-04 09:09:12 -0600

    Estilos modificados

--- src/SIGESRHI/AdminBundle/Form/AccesoType.php
+++ src/SIGESRHI/AdminBundle/Form/AccesoType.php
@@ -11,7 +11,7 @@ class AccesoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nombrepagina')
+            ->add('nombrepagina',null,array('required'=>'required'))
             ->add('ruta')
             ->add('idrol')
             ->add('idmodulo')
